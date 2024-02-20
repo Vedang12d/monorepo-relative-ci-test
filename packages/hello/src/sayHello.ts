8e106db1d1bccf0b9b0e5c547c1b4e57d906ce8c
Vedang Dadape <81163049+Vedang12d@users.noreply.github.com>
2024-02-20 19:09:14 +0000

    fewifh

--- packages/hello/src/sayHello.ts
+++ packages/hello/src/sayHello.ts
@@ -1,6 +1,7 @@
 import { speak } from '@jhasuraj01/speak'
 
 export const sayHello = (speaker: string): string => {
+  console.log("eigni");
   return speak({
     speaker,
     message: 'Something'
